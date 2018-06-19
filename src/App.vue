9a7f4cdf664f26943e6cf9e2458350e25085cfee
DimaHealth <dima_khodich@mail.ru>
2018-06-19 07:57:32 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template >
+  <div id="app">
+    <div class="register"></div>
+    <div class="register__item"></div>
+    <img src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass" >
+
+  @import "./sass/register.sass";
+
+</style>
